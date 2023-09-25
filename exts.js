afcb7c04c7e7f73df3040967369e73f8ce7180cb
Chris Zuber <shgysk8zer0@gmail.com>
2023-09-24 19:12:18 -0700

    Add the missing constant modules

---
+++ exts.js
@@ -0,0 +1,82 @@
+/**
+ * @copyright 2023 Chris Zuber <admin@kernvalley.us>
+ */
+
+// Text and Code
+export const TEXT = ['.txt'];
+export const PLAIN_TEXT = TEXT;
+export const HTML = ['.html', '.htm'];
+export const CSS = ['.css'];
+export const JS = ['.js'];
+export const JSON = ['.json'];
+export const JSON_LD = ['.jsonld'];
+export const WEB_APP_MANIFEST = ['.webmanifest'];
+export const XML = ['.xml'];
+export const RSS_XML = ['.rss'];
+export const RSS = RSS_XML;
+export const ATOM_XML = ['.atom'];
+export const ATOM = ATOM_XML;
+export const XSLT = ['.xslt'];
+export const XHTML = ['.xhtml'];
+export const MARKDOWN = ['.md'];
+
+// Images
+export const JPEG = ['.jpeg', '.jpg'];
+export const PNG = ['.png'];
+export const GIF = ['.gif'];
+export const WEBP = ['.webp'];
+export const SVG = ['.svg'];
+export const GIMP = ['.xcf'];
+
+// Audio
+export const OGG_AUDIO = ['.ogg', '.oga'];
+export const OGA = OGG_AUDIO;
+export const MP3 = ['.mp3'];
+export const AAC = ['.aac'];
+export const FLAC = ['.flac'];
+export const WAV = ['.wav'];
+export const MIDI = ['.midi'];
+
+// Video
+export const WEBM = ['.webm'];
+export const OGG_VIDEO = ['.ogg', '.ogv'];
+export const OGV = OGG_VIDEO;
+export const MP4 = ['.mp4'];
+export const WEBVTT = ['.vtt'];
+
+// Fonts
+export const WOFF = ['.woff'];
+export const WOFF2 = ['.woff2'];
+export const TTF = ['.ttf'];
+export const OTF = ['.otf'];
+export const SVG_FONT = ['.svg'];
+
+// Office Documents
+export const CSV = ['.csv'];
+export const RTF = ['.rtf'];
+export const PDF = ['.pdf'];
+export const ICAL = ['.ics'];
+export const VCARD = ['.vcf'];
+
+// Libre Office and Open Office
+export const LIBRE_OFFICE_TEXT = ['.odt'];
+export const LIBRE_OFFICE_SPREADSHEET = ['.ods'];
+export const LIBRE_OFFICE_PRESENTATION = ['.odp'];
+
+// Microsoft Office
+export const MS_WORD = ['.doc'];
+export const MS_EXCEL = ['.xls'];
+export const MS_POWERPOINT = ['.ppt'];
+
+// Compressed Archives
+export const GZIP = ['.gz'];
+export const ZIP = ['.zip'];
+
+// Adobe Types
+export const ADOBE_PDF = PDF;
+export const ADOBE_POSTSCRIPT = ['.ps'];
+export const ADOBE_ILLUSTRATOR = ['.ai'];
+export const ADOBE_PHOTOSHOP = ['.psd'];
+export const ADOBE_FLASH = ['.swf'];
+export const ADOBE_PREMIERE = ['.pr'];
+export const ADOBE_AFTER_EFFECTS = ['.aep'];
