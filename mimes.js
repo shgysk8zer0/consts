9711d578bc71d87d716182dcd2216e361626123f
Chris Zuber <shgysk8zer0@gmail.com>
2023-09-26 10:22:37 -0700

    Add missing mimes and extensions

--- mimes.js
+++ mimes.js
@@ -19,8 +19,12 @@ export const ATOM = ATOM_XML;
 export const XSLT = 'application/xslt+xml';
 export const XHTML = 'application/xhtml+xml';
 export const MATH_ML = 'application/mathml+xml';
+export const SQL = 'application/sql';
 export const MARKDOWN = 'text/markdown';
 export const YAML = 'application/x-yaml';
+export const TOML = 'application/toml';
+export const PHP = 'application/x-httpd-php';
+export const BASH = 'application/x-sh';
 
 // Images
 export const JPEG = 'image/jpeg';
@@ -29,6 +33,8 @@ export const GIF = 'image/gif';
 export const WEBP = 'image/webp';
 export const SVG = 'image/svg+xml';
 export const AVIF = 'image/avif';
+export const ICO = 'image/vnd.microsoft.icon';
+export const TIFF = 'image/tiff';
 export const GIMP = 'application/x-gimp';
 
 // iOS Images
@@ -40,10 +46,12 @@ export const OGG_AUDIO = 'audio/ogg';
 export const OGA = OGG_AUDIO;
 export const MP3 = 'audio/mpeg';
 export const AAC = 'audio/aac';
+export const OPUS = 'audio/opus';
 export const FLAC = 'audio/flac';
 export const WAV = 'audio/wav';
 export const MIDI = 'audio/midi';
 export const M4A = 'audio/mp4';
+export const WEBA = 'audio/webm';
 
 // Video
 export const WEBM = 'video/webm';
@@ -51,15 +59,17 @@ export const OGG_VIDEO = 'video/ogg';
 export const OGV = OGG_VIDEO;
 export const MP4 = 'video/mp4';
 export const M4V = MP4;
+export const MPEG = 'video/mpeg';
 export const QUICKTIME = 'video/quicktime';
 export const WEBVTT = 'text/vtt';
 
 // Fonts
-export const WOFF = 'application/font-woff';
+export const WOFF = 'font/woff';
 export const WOFF2 = 'font/woff2';
-export const TTF = 'application/font-sfnt';
-export const OTF = 'application/font-sfnt';
-export const SVG_FONT = 'application/font-svg';
+export const TTF = 'font/ttf';
+export const OTF = 'font/otf';
+export const SVG_FONT = SVG;
+export const EOT = 'application/vnd.ms-fontobject';
 
 // Office Documents
 export const CSV = 'text/csv';
@@ -74,13 +84,21 @@ export const LIBRE_OFFICE_SPREADSHEET = 'application/vnd.oasis.opendocument.spre
 export const LIBRE_OFFICE_PRESENTATION = 'application/vnd.oasis.opendocument.presentation';
 
 // Microsoft Office
-export const MS_WORD = 'application/msword';
-export const MS_EXCEL = 'application/vnd.ms-excel';
-export const MS_POWERPOINT = 'application/vnd.ms-powerpoint';
+export const MS_DOCX = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document';
+export const MS_DOC = 'application/msword';
+export const MS_WORD = MS_DOCX;
+export const MS_XLSX = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
+export const MS_XLS = 'application/vnd.ms-excel';
+export const MS_EXCEL = MS_XLSX;
+export const MS_PPTX = 'application/vnd.openxmlformats-officedocument.presentationml.presentation';
+export const MS_PPT = 'application/vnd.ms-powerpoint';
+export const MS_POWERPOINT = MS_PPTX;
 
 // Compressed Archives
 export const GZIP = 'application/gzip';
 export const ZIP = 'application/zip';
+export const TAR = 'application/x-tar';
+export const SEVEN_ZIP = 'application/x-7z-compressed';
 
 // Adobe Types
 export const ADOBE_PDF = PDF;
